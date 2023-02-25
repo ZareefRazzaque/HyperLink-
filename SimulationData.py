f4f04efb53f61f3224483b2767d8781191e29903
Zareef Razzaque <zareefrazzaque@outlook.com>
2023-02-25 20:46:21 +0000

    swapped out randoms with functions, funcitons will need to be filled in at some point

--- SimulationData.py
+++ SimulationData.py
@@ -1,14 +1,69 @@
 import csv
 import random
 import time
+from datetime import datetime
 
-# open the csv file
 
-    # create a csv writer object
+#these are the functions that create the simulation data for each sensor we are tracking 
+
+def generatePodTemperature():
+  return random.randint(0,100)
+  
+  
+  
+def generatePRMS():
+  return random.randint(0,100)
+
+
+
+def generateIRMS():
+  return random.randint(0,100)
+  
+  
+  
+def generateSpeed():
+  return random.randint(0,100)
+  
+  
+  
+def generateBatteryTemp():
+  return random.randint(0,100)
+  
+  
+  
+def generateVoltage():
+  return random.randint(0,100)
+  
+  
+
+def generateDischargeRate():
+  return random.randint(0,100)
+
+
+
+
+
+
+
+
+
+  ##########################################################################################################################################  
     
-    
-    # create 5 columns
+    # creates 5 columns
     # currently all generated numbers are random between 0 to 99 
+    
+with open('simul1.csv', 'w') as csvfile: #upon starting, the program creates a fresh csv file
+  csvwriter = csv.writer(csvfile)
+  csvwriter.writerow(["Pod Temperature",
+                      "PRMS",
+                      "IRMS",
+                      "Speed",
+                      "Battery Temperature",
+                      "Voltage",
+                      "Discharge Rate",
+                      datetime.now()
+                      ])
+  csvfile.close() 
     
 while(True):
                                                       #the program opens the csv file here
@@ -16,13 +71,14 @@ while(True):
     
     
     csvwriter = csv.writer(csvfile)
-    csvwriter.writerow([random.randint(0, 100),
-                        random.randint(0, 100),
-                        random.randint(0,100),
-                        random.randint(0,100),
-                        random.randint(0,100),
-                        random.randint(0,100),
-                        random.randint(0,100)
+    csvwriter.writerow([
+                        generatePodTemperature(),
+                        generatePRMS(),
+                        generateIRMS(),
+                        generateSpeed(),
+                        generateBatteryTemp(),
+                        generateVoltage(),
+                        generateDischargeRate()
                         ])
     csvfile.close()                                  #we close the csv file to save the generated data properly
                                                      #so that other applicaions may access the data
