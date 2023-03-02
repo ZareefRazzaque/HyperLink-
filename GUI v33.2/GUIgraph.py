245ff554d56fbd7a2b16cce06ef5888060496ee0
Zareef Razzaque <zareefrazzaque@outlook.com>
2023-03-02 21:47:23 +0000

    working towards getting the graphs fully running

--- GUI v33.2/GUIgraph.py
+++ GUI v33.2/GUIgraph.py
@@ -2,6 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import pyqtgraph
 
 
+
 class GUIGraph():
     def __init__(self, name, view, recordnumber ):
         
@@ -19,11 +20,17 @@ class GUIGraph():
         self.graph.setBackground('w')
         self.graph.plot(  [0 for i in range(0,len(self.datashown))],   self.datashown,   pen= self.pen)
         
-        
+
         
         
     ############################################################################################################
     #functions 
+    
+    def pointernext(self):
+        self.pointer += 1 
+        if self.pointer == len(self.datashown):
+            self.pointer = 0
+    
     def plot(self, data):
         self.clear
         self.graph.plot([i for i in range(len(data))], data, pen = self.pen)
@@ -32,7 +39,8 @@ class GUIGraph():
                 #how the plot function works is through through drawing a line
                 #the first array details the x axis of the lines points beiing drawn
                 #whist the second line is the y asis of hte lines points 
-        
+    
+
         
     def clear(self):
         self.graph.clear()
@@ -40,7 +48,26 @@ class GUIGraph():
         self.datashown = [0 for i in range(len(self.datashown))]
     
     def update(self, data):
-        pass
+        self.datashown[self.pointer] = data
+        self.pointernext()
+        self.graph.clear()
+        
+        for i in range (len(self.datashown)):
+            
+            self.graph.plot( 
+                            [i for i in range(0,len(self.datashown))],
+                            self.datashown[self.pointer:]+self.datashown[:self.pointer],
+                            pen = self.pen
+                            )
+        
+    def dataUpdate(self):
+        print("testing")
+        with open('simul1.csv', 'r') as csvfile:
+            data = csvfile.readlines()
+            print(data[self.recordnumber])
+            csvfile.close()
+            
+        
     
 
         
